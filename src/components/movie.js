b03274b6ef22d5f5cea5364e9a919d948b2f0747
Johannes <johannes.widkopp@gmail.com>
2024-02-13 08:31:10 +0200

    appi hakee trendaavat ohjelmat postereina

---
+++ src/components/movie.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+
+function Movie () {
+    const [movielist, setmovielist] = useState([])
+
+    const getMovie = () => {
+        fetch("https://api.themoviedb.org/3/trending/all/week?api_key=6d041dad9572ee4b721003aec0574a6e")
+        .then(res => res.json())
+        .then(json => {
+            console.log(json.results)
+            setmovielist(json.results)
+        })
+    }    
+
+    useEffect(() => {
+        getMovie()
+    }, [])
+
+    return (
+        <div>
+            {movielist.map((movie) => (
+                <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.title} />
+            ))}
+        </div>
+    )
+}
+
+export default Movie
